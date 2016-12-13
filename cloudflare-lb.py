61fb1903ef8627581b19b677d8b3eda106a659d0
inventivetalent <inventivetalent@inventivegames.de>
2016-12-13 18:59:05 +0100

    update to Cloudflare API v4 & fix some stuff

--- cloudflare-lb.py
+++ cloudflare-lb.py
@@ -2,8 +2,12 @@
 
 import json
 import urllib
+import httplib2
 import time
 import random
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import requests
 
 HOSTS = [
     ['127.0.0.1', 'A'],
@@ -14,79 +18,122 @@ HOSTS = [
 ]  # Your A/AAAA value, i.e. 192.168.1.10
 PROTO = 'http'  # http or https
 PORT = 80  # Defaults to port 80
+EMAIL = ''  # Your CloudFlare email address
 API = ''  # Your CloudFlare client API key found at https://www.cloudflare.com/my-account
-EMAIL = 'example@example.com'  # Your CloudFlare email address
-RECORD = 'lb'  # where you want the load balancer
+RECORD = 'lb.example.com'  # where you want the load balancer
+ZONE = 'example.com'  # Name of the Zone you want to edit (typically the domain name)
 DOMAIN = 'example.com'  # Use DOMAIN if this is for the root domain
 TTL = 1  # Set TTL
-RECS = []  # RECORDS
 INTERVAL = 60  # how long we wait between runs
 
-
-def call_api(params):
-    go = urllib.urlopen("https://www.cloudflare.com/api_json.html", params)
-    return go.read()
+ZONE_ID = ''
+RECS = []  # RECORDS
 
 
-def get_recs():
-    print("\nGetting data from CloudFlare")
-    rec = json.loads(call_api(urllib.urlencode({'a': 'rec_load_all', 'tkn': API, 'email': EMAIL, 'z': DOMAIN})))
-    if rec['result'] == "success":
-        return rec['response']['recs']['objs']
+def call_api(route, params, method="GET"):
+    url = "https://api.cloudflare.com/client/v4" + route
+    headers = {
+        "X-Auth-Email": EMAIL,
+        "X-Auth-Key": API,
+        "Content-Type": "application/json",
+        "User-Agent": "Cloudflare-LoadBalancer"
+    }
+
+    response = requests.request(method=method, url=url, params=params, headers=headers)
+
+    return json.loads(response.text)
+
+
+def get_zone(name):
+    print("\nLooking up zone with name " + name + "...")
+    zones = call_api("/zones", {"name": DOMAIN, "status": "active"})
+    print(zones)
+    if len(zones['result']) == 0:
+        print("\nCouldn't find any zones")
+        return False
+
+    elif len(zones['result']) == 1:
+        zone = zones['result'][0]
+        print("\nFound zone '" + zone['name'] + "' with id '" + zone['id'] + "'")
+        return zone['id']
+
+
+def get_recs(zone_id):
+    print("\nLoading DNS records")
+    rec = call_api("/zones/" + zone_id + "/dns_records", {"name": DOMAIN, "per_page": 100})
+    print(rec)
+    if rec['success']:
+        print(rec['result'])
+        return rec['result']
     else:
         return False
 
 
-def del_rec(rec_id, host):
-    result = json.loads(call_api(urllib.urlencode({'a': 'rec_delete', 'tkn': API, 'email': EMAIL, 'z': DOMAIN, 'id': rec_id})))
-    if result['result'] == 'success':
+def get_rec(zone_id, name, type, content):
+    print("\nLooking up " + type + " record '" + name + "' with '" + content + "'")
+    rec = call_api("/zones/" + zone_id + "/dns_records", {"name": name, "content": content, "type": type})
+    print(rec)
+    if rec['success']:
+        print(rec['result'])
+        return rec['result']
+    else:
+        return False
+
+
+def del_rec(zone_id, rec_id, host):
+    result = call_api("/zones/" + zone_id + "/dns_record/" + rec_id, {}, "DELETE")
+    if result['success']:
         print('Removing:' + host)
     else:
-        print('Remove Failed: ' + host + '. ' + result['msg'])
+        print('Remove Failed: ' + host + '. ')
+        print(result['messages'])
 
 
 def add_rec(rec):
-    result_add = json.loads(call_api(urllib.urlencode({'a': 'rec_new', 'tkn': API, 'email': EMAIL, 'z': DOMAIN, 'name': RECORD, 'content': rec[0], 'type': rec[1], 'ttl': 1})))
-    if result_add['result'] == 'success':
-        result_edit = json.loads(call_api(urllib.urlencode({'a': 'rec_edit', 'tkn': API, 'email': EMAIL, 'z': DOMAIN, 'name': RECORD, 'content': rec[0], 'type': rec[1], 'ttl': 1, 'id': result_add['response']['rec']['obj']['rec_id'], 'service_mode': 1})))
-        if result_edit['result'] == 'success':
-            print('Adding: ' + rec[0])
-            return True
-        else:
-            del_rec(result_add['response']['rec']['obj']['rec_id'], rec)  # failed to orange cloud
-            print('Add Failed: {0}. {1}'.format(rec[0], result_edit['msg']))
-            return False
+    result_add = call_api("/zones", {"name": RECORD, "content": rec[0], "type": rec[1]}, "POST")
+    if result_add['success']:
+        # TODO: maybe modify the "orange cloud" if needed
+        return True
 
-    print('Add Failed: {0}. {1}'.format(rec[0], result_add['msg']))
+    print('Add Failed: {0}.'.format(rec[0]))
+    print(result_add['messages'])
     return False
 
 
 def healthcheck(host):
+    # TODO: add IP exception to Cloudflare firewall
     try:
-        run = urllib.urlopen("{0}://{1}:{2}/".format(PROTO, host[0], str(PORT)))
-        if not get_rec_id(RECORD, host[0]):  # needs to be added
+        url = "{0}://{1}:{2}/".format(PROTO, host[0], str(PORT))
+        print("\nTesting connection to " + url)
+        run = urlopen(url)
+        print("-> ONLINE")
+        if not get_rec(ZONE_ID, RECORD, host[1], host[0]):  # needs to be added
             add_rec(host)
         else:
             print(host[0] + ': Passed')
-    except IOError:  # we were not able to do what was needed
-        rec_id = get_rec_id(RECORD, host[0])  # get the id of the record
+    except IOError as err:  # we were not able to do what was needed
+        print("-> OFFLINE", err)
+        rec = get_rec(ZONE_ID, RECORD, host[1], host[0])[0]  # get the id of the record (this SHOULD only return one record)
+        rec_id = rec['id']
         if rec_id is not False:
             print(host[0] + ': Removing Host')
-            del_rec(rec_id, host[0])
+            del_rec(ZONE_ID, rec_id, host[0])
         else:
             print(host[0] + ': Still dead')
 
 
 def get_rec_id(name, host):
-    for y in RECS:
-        if y['display_name'] == name and y['content'] == host and (y['type'] == "A" or y['type'] == "AAAA"):
-            return y['rec_id']
+    for rec in RECS:
+        if rec['name'] == name and rec['content'] == host and (rec['type'] == "A" or rec['type'] == "AAAA"):
+            return rec['id']
     return False
 
 
 if __name__ == "__main__":
     while True:
-        RECS, start_time = get_recs(), time.time()
+        ZONE_ID = get_zone(ZONE)
+        RECS = get_recs(ZONE_ID)
+        start_time = time.time()
         random.shuffle(HOSTS)
 
         for host in HOSTS:
